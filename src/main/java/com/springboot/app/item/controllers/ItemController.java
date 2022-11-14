7d1af94dec6c03e979cdd80d25daa0eeff7d045f
Charly <charly81.zirpoli@gmail.com>
2022-11-14 14:27:38 -0300

    Se agregan configuraciones de resilience4j

--- src/main/java/com/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/springboot/app/item/controllers/ItemController.java
@@ -2,6 +2,8 @@ package com.springboot.app.item.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
@@ -13,10 +15,11 @@ import com.springboot.app.item.models.Item;
 import com.springboot.app.item.models.Producto;
 import com.springboot.app.item.models.service.ItemService;
 
-
 @RestController
 public class ItemController {
-	
+
+	private final Logger logger = LoggerFactory.getLogger(ItemController.class);
+
 	@Autowired
 	private CircuitBreakerFactory cbFactory;
 
@@ -32,17 +35,18 @@ public class ItemController {
 //	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
-		return cbFactory.create("items")
-				.run(() -> itemService.findById(id, cantidad), e -> metodoAlternativo(id, cantidad));
+		return cbFactory.create("items").run(() -> itemService.findById(id, cantidad)/*,
+				e -> metodoAlternativo(id, cantidad, e)*/);
 	}
 
-	public Item metodoAlternativo(Long id, Integer cantidad) {
+	public Item metodoAlternativo(Long id, Integer cantidad, Throwable e) {
+		logger.info(e.getMessage());
 		Item item = new Item();
 		Producto producto = new Producto();
 
 		item.setCantidad(cantidad);
 		producto.setId(id);
-		producto.setNombre("Camara Sony");
+		producto.setNombre("Camara Sony 111");
 		producto.setPrecio(500.00);
 		item.setProducto(producto);
 
