c61b939c40dc6c6049554e0b3610b7d6ee2c1ce3
Charly1981 <charly81.zirpoli@gmail.com>
2022-11-15 16:02:36 -0300

    Se agrega anotacion TimeLimiter

--- src/main/java/com/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/springboot/app/item/controllers/ItemController.java
@@ -1,6 +1,7 @@
 package com.springboot.app.item.controllers;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +17,7 @@ import com.springboot.app.item.models.Producto;
 import com.springboot.app.item.models.service.ItemService;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 
 @RestController
 public class ItemController {
@@ -39,11 +41,18 @@ public class ItemController {
 		return cbFactory.create("items").run(() -> itemService.findById(id, cantidad),
 				e -> metodoAlternativo(id, cantidad, e));
 	}
-	
-	@CircuitBreaker(name="items", fallbackMethod = "metodoAlternativo")
+
+	@CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver2/{id}/cantidad/{cantidad}")
 	public Item detalle2(@PathVariable Long id, @PathVariable Integer cantidad) {
-		return  itemService.findById(id, cantidad);
+		return itemService.findById(id, cantidad);
+	}
+
+	@CircuitBreaker(name = "items", fallbackMethod = "metodoAlternativo2")
+	@TimeLimiter(name = "items", fallbackMethod = "metodoAlternativo2")
+	@GetMapping("/ver3/{id}/cantidad/{cantidad}")
+	public CompletableFuture<Item> detalle3(@PathVariable Long id, @PathVariable Integer cantidad) {
+		return CompletableFuture.supplyAsync(() -> itemService.findById(id, cantidad));
 	}
 
 	public Item metodoAlternativo(Long id, Integer cantidad, Throwable e) {
@@ -58,6 +67,21 @@ public class ItemController {
 		item.setProducto(producto);
 
 		return item;
+
+	}
+
+	public CompletableFuture<Item> metodoAlternativo2(Long id, Integer cantidad, Throwable e) {
+		logger.info(e.getMessage());
+		Item item = new Item();
+		Producto producto = new Producto();
+
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("Camara Sony 2222");
+		producto.setPrecio(500.00);
+		item.setProducto(producto);
+
+		return CompletableFuture.supplyAsync(() -> item);
 
 	}
 }
